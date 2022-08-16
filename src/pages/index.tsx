b7ad176ed6d3642450dd5e2ca0969d0812b89345
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-08-16 20:45:38 -0300

    fix: fixed sintaxe error on index.tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home(props: HomeProps) {
     <ChallengesProvider
       level={props.level}
       currentExperience={props.currentExperience}
-      challengesComleted={props.challengesCompleted}
+      challengesCompleted={props.challengesCompleted}
     >
       <div className={styles.container}>
         <Head>
